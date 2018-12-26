d879d6da8fa7a96e1cf020ff382482d3dedbf7b8
xybersolve <1531070+xybersolve@users.noreply.github.com>
2018-12-26 13:54:55 -0800

    update data.tf

--- data.tf
+++ data.tf
@@ -2,7 +2,17 @@ data "aws_route53_zone" "dns_host" {
   name = "${var.host_domain}"
   private_zone = false
 }
+data "aws_instance" "this" {
+  # tags {
+  #   Name = "${var.instance_name_tag}"
+  # }
+  filter {
+    name   = "tag:Name"
+    values = ["${var.instance_name_tag}"]
+  }
+}
 
 locals {
   host_zone_id = "${data.aws_route53_zone.dns_host.zone_id}"
+  public_ip = "${data.aws_instance.this.public_ip}"
 }
